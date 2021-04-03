37ee10d4af509872e417168be69e7171f70fc576
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-04-03 14:19:49 +0530

    fixed dark mode bug

--- frontend/home.js
+++ frontend/home.js
@@ -1,16 +1,23 @@
 window.onload= DarkMode()
 
 function DarkMode() {
+	var all_Ques_class = document.getElementsByClassName('question-card');
 	body_elem= document.querySelector('body');
 	nav_el= document.getElementById('Nav-dark');
 	if (document.getElementById('DarkToggle').checked== true){
 	  	body_elem.style.backgroundColor = 'black';
 	    body_elem.style.color = 'white';
 	    nav_el.className="navbar navbar-expand-lg navbar-dark bg-dark";
+	    for (var i = 0; i < all_Ques_class.length; i++) {
+	    	all_Ques_class[i].style.color= 'black';
 	    }
+	}
 	else{
 	  	body_elem.style.backgroundColor = "";
 	    body_elem.style.color = "";
 	  	nav_el.className="navbar navbar-expand-lg navbar-light bg-light";
+	  	for (var i = 0; i < all_Ques_class.length; i++) {
+	    	all_Ques_class[i].style.color= 'black';
+	    }
 	}
 }
