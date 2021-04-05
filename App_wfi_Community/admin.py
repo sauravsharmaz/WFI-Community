e05c5cec891e330ec9ed5e1fb28f4de400c0704c
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-04-05 19:31:44 +0530

    fixed bug in models migration and added more new models

--- App_wfi_Community/admin.py
+++ App_wfi_Community/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from App_wfi_Community.models import Question, Answer
+from App_wfi_Community.models import Question, Answer, Comment, Upvote, DownVote
 
 # Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display= ('title','AskedBy')
+    search_fields= ('title', 'detail','ask_time','AskedBy__username')
 
+class AnswerAdmin(admin.ModelAdmin):
+    list_display= ('detail','related_question','AnsGiver')
+    search_fields= ('detail','related_question__title','AnsGiver__username')
 
-admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Comment)
+admin.site.register(Upvote)
+admin.site.register(DownVote)
--- App_wfi_Community/models.py
+++ App_wfi_Community/models.py
@@ -6,21 +6,34 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     """(User Question Model)"""
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    AskedBy = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=500)
     detail = models.TextField()
     ask_time = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
 
-
-
 class Answer(models.Model):
     """(User Answer Model)"""
+    AnsGiver= models.ForeignKey(User, on_delete= models.CASCADE)
     related_question = models.ForeignKey(Question, on_delete=models.CASCADE)
     detail = models.TextField()
     post_time = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.detail
 
+class Comment(models.Model):
+    """(User Comment Model)"""
+    answer= models.ForeignKey(Answer, on_delete= models.CASCADE)
+    commented_By= models.ForeignKey(User, on_delete= models.CASCADE)
+    post_time = models.DateTimeField(auto_now_add=True)
 
+class Upvote(models.Model):
+    """(User Upvote Model)"""
+    answer= models.ForeignKey(Answer, on_delete= models.CASCADE)
+    Upvote_By= models.ForeignKey(User, on_delete= models.CASCADE)
+
+class DownVote(models.Model):
+    """(User DownVote Model)"""
+    answer= models.ForeignKey(Answer, on_delete= models.CASCADE)
+    DownVote_By= models.ForeignKey(User, on_delete= models.CASCADE)
