1129b140ecc59c8787494aa335989b76aef9a596
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-04-06 08:55:44 +0530

    added pagination feature to reduce the amount of questions on a single page

--- App_wfi_Community/urls.py
+++ App_wfi_Community/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from App_wfi_Community import views
 
 urlpatterns = [
     path('',views.index),
-    path('answers/<str:qtn_title>',views.ans),
+    # path('/answers/<slug:qtn_title>',views.ans),
+    path('answers/<slug:idl>', views.ans),
 ]
--- App_wfi_Community/views.py
+++ App_wfi_Community/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.http import request
+# import the models
 from .models import Question, Answer
+# import paginator for pagination
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
-  all_qns= Question.objects.all()
+  request.session.flush()
+  # get all objects of question model
+  all_qns= Question.objects.all().order_by('id')
+  # passing all questions to paginator with 4 question for one page
+  paginator= Paginator(all_qns, 4, orphans=2)
+  # get page no from home.html element with name= 'page'
+  page_number= request.GET.get('page')
+  # making page object
+  page_object= paginator.get_page(page_number)
+  # get all answer objects
   all_ans= Answer.objects.all()
-  qt_title= request.GET.get('question_title','no item')
-  print('form el ===>',qt_title)
-  data={'all_ans':all_ans,'all_qns':all_qns}
+  # pass all the data to dictionary
+  data={'all_ans':all_ans,'all_qns':all_qns,'page_object':page_object}
   return render(request,'home.html', data) 
 
 
-def ans(request,qtn_title):
+def ans(request,idl):
+  request.session.flush()
+  print("the slig param is ====>",idl)
   # all_ans= Answer.objects.all()
-  # mainAns= Answer.objects.get(related_question= qtn_title)
-  # print(mainAns)
+  # mainAns= Answer.objects.get(related_question__title= qtn_title)
+  # print("the filter is working and the value is=====>",mainAns)
+  # mainAn= Answer.objects.filter(related_question__title= "what is django.?")
+  # print("the answer is ===> ", mainAn)
   return render(request, 'answers.html')
