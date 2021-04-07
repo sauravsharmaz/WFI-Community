15c0eb8af7c7a8055765215774471ffd5a8708d0
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-04-07 12:50:54 +0530

    Added icons for upvotes & DownVotes

--- App_wfi_Community/models.py
+++ App_wfi_Community/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # importing user
 from django.contrib.auth.models import User
 
-
 class Question(models.Model):
     """(User Question Model)"""
     AskedBy = models.ForeignKey(User, on_delete=models.CASCADE, default="Anonymous User")
