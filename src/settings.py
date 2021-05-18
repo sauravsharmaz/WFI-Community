9d27a20acad7f8e46a81dbe84914639501d469a3
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-05-18 15:57:43 +0530

    removed sqlite3 and added postgresql 13
    a new database(postgresql 13) has been added & named as WFI-DB

--- src/settings.py
+++ src/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'WFI-DB',
+        'USER': 'postgres',
+        'PASSWORD': '5555',
+        'HOST': 'localhost',
+        'PORT': '5432',        
     }
 }
 
