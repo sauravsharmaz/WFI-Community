d548ec71b239d48babbc204aa37b1f2e0b73d083
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-07-11 01:00:49 -0700

    configured for hosting heroku

--- src/wsgi.py
+++ src/wsgi.py
@@ -6,6 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
+from dj_static import Cling # for hosting
+
 
 import os
 
@@ -13,4 +15,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
